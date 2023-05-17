a09d22fef070e2eac8ee2f52f8db85d64d6d3976
jpdravila <jpdravila@gmail.com>
2023-05-17 19:51:54 -0400

    Dividing test, Part. 02,

--- src/test/java/br/io/jpdravila/servicos/LocacaoServiceTeste.java
+++ src/test/java/br/io/jpdravila/servicos/LocacaoServiceTeste.java
@@ -5,7 +5,10 @@ import br.io.jpdravila.entidades.Locacao;
 import br.io.jpdravila.entidades.Usuario;
 import br.io.jpdravila.utils.DataUtils;
 import org.hamcrest.CoreMatchers;
+import org.junit.Rule;
 import org.junit.jupiter.api.Test;
+import org.junit.rules.ErrorCollector;
+
 import java.util.Date;
 
 import static java.util.function.Predicate.not;
@@ -15,6 +18,9 @@ import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
 
 public class LocacaoServiceTeste {
+
+    @Rule
+    public ErrorCollector error = new ErrorCollector();
 
     @Test
     public void teste() {
@@ -33,10 +39,14 @@ public class LocacaoServiceTeste {
 //        Assert.assertEquals(5.0, locacao.getValor(), 0.01);
 //        Assert.assertTrue(DataUtils.isMesmaData(locacao.getDataRetorno(), DataUtils.obterDataComDiferencaDias(1)));
 
-        assertThat(locacao.getValor(), is(equalTo(5.0)));
-        assertThat(locacao.getValor(), is(CoreMatchers.not(5.0)));
-        assertThat(DataUtils.isMesmaData(locacao.getDataLocacao(), new Date()), is(true));
-        assertThat(DataUtils.isMesmaData(locacao.getDataRetorno(), DataUtils.obterDataComDiferencaDias(1)), is(true));
+//        assertThat(locacao.getValor(), is(equalTo(5.0)));
+//        assertThat(DataUtils.isMesmaData(locacao.getDataLocacao(), new Date()), is(true));
+//        assertThat(DataUtils.isMesmaData(locacao.getDataRetorno(), DataUtils.obterDataComDiferencaDias(1)), is(true));
+
+        //Compilando erros C 5.0 - E 6.0 / C true - E false
+        error.checkThat(locacao.getValor(), is(equalTo(6.0)));
+        error.checkThat(DataUtils.isMesmaData(locacao.getDataLocacao(), new Date()), is(true));
+        error.checkThat(DataUtils.isMesmaData(locacao.getDataRetorno(), DataUtils.obterDataComDiferencaDias(1)), is(false));
 
     }
 
